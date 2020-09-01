7ca6f1fdcc8fdeba572c9e2a267ca03fdd78103e
hanlee <me@hanlee.co>
2020-09-01 18:04:11 +0900

    feat: handle layout & router-view

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <div class="flex flex-wrap h-full">
+    <Header />
+    <SideMenu />
+    <div class="flex-1 px-4 py-10 text-center text-white">
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
