8952a96b150fd8fb29687e22126c8b807c0b4cc4
hanlee <me@hanlee.co>
2020-09-01 18:02:27 +0900

    feat: add vue-router & purge-icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import routes from 'voie-pages'
+
 import App from './App.vue'
 import './index.css'
+import '@purge-icons/generated'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+app.use(router)
+
+app.mount('#app')
