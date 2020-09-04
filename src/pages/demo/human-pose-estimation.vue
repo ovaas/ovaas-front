bb933468e86e3b9aaa0086498d9b77e0c4c9d90a
hanlee <me@hanlee.co>
2020-09-04 14:25:29 +0900

    feat: new demo page

---
+++ src/pages/demo/human-pose-estimation.vue
@@ -0,0 +1,27 @@
+<template>
+  <MainContant title="Demos">
+    <div class="flex-auto flex w-full items-stretch">
+      <div class="w-2/3 bg-white"></div>
+      <div class="w-1/3">
+        <FormulateInput
+          type="file"
+          name="file"
+          label="Select your documents to upload"
+          help="Select one or more PDFs to upload"
+          validation="mime:application/pdf"
+          multiple
+        />
+      </div>
+    </div>
+  </MainContant>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
