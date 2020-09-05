4d2b7847ef9eac51c1c9776c5c003083de0cdc4c
hanlee <me@hanlee.co>
2020-09-05 17:43:17 +0900

    fix: @braid/vue-formulate only work on vue2

--- src/pages/demo/human-pose-estimation.vue
+++ src/pages/demo/human-pose-estimation.vue
@@ -3,14 +3,6 @@
     <div class="flex-auto flex w-full items-stretch">
       <div class="w-2/3 bg-white"></div>
       <div class="w-1/3">
-        <FormulateInput
-          type="file"
-          name="file"
-          label="Select your documents to upload"
-          help="Select one or more PDFs to upload"
-          validation="mime:application/pdf"
-          multiple
-        />
       </div>
     </div>
   </MainContant>
