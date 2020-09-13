e79bf344eb858e7535db09301eef26dcb4dd27bd
hanlee <me@hanlee.co>
2020-09-14 01:02:31 +0900

    feat: add class attrs & go back button

--- src/components/MainContant.vue
+++ src/components/MainContant.vue
@@ -1,8 +1,9 @@
 <template>
-  <main class="flex flex-col items-start w-5/6 min-h-full mx-auto px-4 py-10 text-white">
+  <main :class="$attrs.class ? $attrs.class : 'w-5/6'" class="flex flex-col items-start min-h-full mx-auto px-4 py-10 text-white relative">
     <h1 class="text-4xl font-bold pb-6">
       {{ title }}
     </h1>
+    <Icon v-if="backBtn" class="absolute transform -translate-x-12 translate-y-1 iconify mb-1 hover:text-gray-300" icon="bx:bx-left-arrow-circle" size="2" @click="$router.go(-1)" />
     <slot></slot>
   </main>
 </template>
@@ -13,6 +14,10 @@ export default {
     title: {
       type: String,
       require: true
+    },
+    backBtn: {
+      type: Boolean,
+      default: false
     }
   }
 }
