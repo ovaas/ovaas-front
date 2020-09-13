0dc5f78d0d2ff0ff4b6aae6801ed45317a2f86d6
hanlee <me@hanlee.co>
2020-09-14 01:00:07 +0900

    feat: add demo data model

--- src/components/DemoCard.vue
+++ src/components/DemoCard.vue
@@ -1,30 +1,40 @@
 <template>
-  <div class="w-full h-full p-4 bg-white bg-opacity-15 rounded-lg">
+  <div class="w-full h-full p-4 bg-white bg-opacity-15 rounded-lg" @click="$router.push(demo.path)">
     <div class="rounded-lg overflow-hidden h-3/5 w-full">
-      <img src="/~/assets/human-pose-estimation-0001.png" alt="human-pose-estimation" class="object-cover w-full">
+      <img
+        :src="demo.image"
+        alt="human-pose-estimation"
+        class="object-cover w-full h-full"
+      >
     </div>
-    <div class="text-left p-3">
-      <h2 class="text-xl font-medium">
-        Title
+    <div class="h-2/5 text-left p-2">
+      <h2 class="text-xl font-medium pt-2">
+        {{ demo.title }}
       </h2>
-      <p class="mt-1">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.
+      <p class="mt-1 w-full">
+        {{ demo.content }}
       </p>
     </div>
   </div>
 </template>
 
-<script>
+<script setup="props">
+import { ref } from "vue";
 export default {
-
+  props: {
+    demo: {
+      type: Object,
+      require: true
+    }
+  }
 }
 </script>
 
 <style scoped>
-p {
-  display: -webkit-box;
-  -webkit-box-orient: vertical;
-  -webkit-line-clamp: 3;
-  overflow: hidden;
-}
+  p {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 3;
+    overflow: hidden;
+  }
 </style>
