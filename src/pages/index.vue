ea239ad349124093f17b9657fc2a3be819781eb2
hanlee <me@hanlee.co>
2020-09-14 00:59:39 +0900

    feat: add demos data model

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,49 @@
 <template>
   <MainContant title="Demos">
     <div class="grid grid-cols-4 gap-4">
-      <DemoCard v-for="n in 10" :key="n" />
+      <DemoCard
+        v-for="demo in demos"
+        :key="demo.path"
+        :demo="demo"
+      />
     </div>
   </MainContant>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+export const demos = [
+  {
+    title: 'Human pose estimation',
+    image: '/~/assets/human-pose-estimation-0001.png',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.',
+    path: '/demo/human-pose-estimation'
+  },
+  {
+    title: 'Human pose estimation',
+    image: '/~/assets/human-pose-estimation-0001.png',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.',
+    path: '/demo/human-pose-estimation'
+  },
+  {
+    title: 'Human pose estimation',
+    image: '/~/assets/human-pose-estimation-0001.png',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.',
+    path: '/demo/human-pose-estimation'
+  },
+  {
+    title: 'Human pose estimation',
+    image: '/~/assets/human-pose-estimation-0001.png',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.',
+    path: '/demo/human-pose-estimation'
+  },
+  {
+    title: 'Human pose estimation',
+    image: '/~/assets/human-pose-estimation-0001.png',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore autem, voluptatem distinctio deleniti cumque accusamus deserunt officiis ducimus minima consequuntur dicta laboriosam modi incidunt, natus architecto iste tempora sit eligendi.',
+    path: '/demo/human-pose-estimation'
+  }
+]
 </script>
 
 <style>
-
 </style>
