5957c880374765db6045426fd35cbee83bd5d9c4
hanlee <me@hanlee.co>
2020-09-16 21:46:35 +0900

    feat: add i18n

---
+++ src/messages.js
@@ -0,0 +1,20 @@
+import en from "../locales/en.json";
+import ja from "../locales/ja.json";
+
+export const messages = {
+  en,
+  ja,
+};
+
+export const locales = Object.keys(messages);
+
+export const languages = [
+  {
+    locale: "en",
+    name: "English",
+  },
+  {
+    locale: "ja",
+    name: "日本語",
+  },
+];
