d59c46b36af151b94521eaab396cb1d9c63646a0
hanlee <me@hanlee.co>
2020-09-17 23:40:53 +0900

    feat: add basic-transition

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center w-14 h-14 bg-white rounded-lg bg-opacity-0 text-white hover:text-black hover:bg-opacity-100">
+  <div class="flex flex-col justify-center items-center w-14 h-14 bg-white rounded-lg bg-opacity-0 text-white hover:text-black hover:bg-opacity-100 basic-transition">
     <Icon class="iconify mb-1" :icon="icon" />
     <p class="text-xs font-bold">
       {{ title }}
