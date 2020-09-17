7ddf0eac1549878f416f970554e6bde52b2766e9
hanlee <me@hanlee.co>
2020-09-17 23:42:14 +0900

    fix: go back button hover effect

--- src/components/MainContant.vue
+++ src/components/MainContant.vue
@@ -3,7 +3,9 @@
     <h1 class="text-4xl font-bold pb-6">
       {{ title }}
     </h1>
-    <Icon v-if="backBtn" class="absolute transform -translate-x-12 translate-y-1 iconify mb-1 hover:text-gray-300　cursor-pointer" icon="bx:bx-left-arrow-circle" :size="2" @click="$router.go(-1)" />
+    <div class="absolute transform -translate-x-12 translate-y-1 hover:text-gray-300">
+      <Icon v-if="backBtn" class="iconify mb-1　cursor-pointer" icon="bx:bx-left-arrow-circle" :size="2" @click="$router.go(-1)" />
+    </div>
     <slot></slot>
   </main>
 </template>
