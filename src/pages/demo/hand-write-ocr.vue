39b1a5c85a12cc9b4d595e885aaf308d68e0df6e
hanlee <me@hanlee.co>
2020-09-18 00:12:09 +0900

    feat: init new demo

---
+++ src/pages/demo/hand-write-ocr.vue
@@ -0,0 +1,18 @@
+<template>
+  <MainContant :title="t('demos.hand-write-ocr.title')" :back-btn="true">
+    <div class="flex-auto h-full w-full bg-gray-100 rounded-lg">
+    </div>
+  </MainContant>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+export { t }
+</script>
+
+<style>
+
+</style>
