4fd7694c465c3638b8e84cad796ece90c54d4e4a
hanlee <me@hanlee.co>
2020-11-29 21:12:58 +0900

    fix: add missing color schema

--- src/logics/store.ts
+++ src/logics/store.ts
@@ -3,3 +3,5 @@ import { useStorage } from '@vueuse/core'
 import { locales } from '../messages'
 
 export const localeSchema = useStorage('locales', 'en') as Ref<locals>
+
+export const colorSchema = useStorage('color-schema', 'auto') as Ref<'auto' | 'dark' | 'light'>
