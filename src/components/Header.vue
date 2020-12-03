2f7b8973a85e2b4e41c04285493f6f5f8ff71fdb
hanlee <me@hanlee.co>
2020-12-03 19:49:38 +0900

    fix: logo path

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <header class="flex-none flex items-center justify-between bg-gray-500 dark:bg-gray-600 w-full h-16 basic-transition">
     <div class="flex h-full items-center">
       <div class="w-18 h-full bg-gray-600 dark:bg-gray-900 flex justify-center items-center">
-        <img src="/favicon.svg" alt="Icon" class="h-8 w-8">
+        <img src="/~/assets/logo.svg" alt="Icon" class="h-8 w-8">
       </div>
       <router-link to="/" class="inline-flex text-white">
         <h1 class="text-3xl font-bold leading-none ml-6">
