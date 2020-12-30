579d93a06a00501fe36dff280eeec152eb5513a4
hanlee <me@hanlee.co>
2020-12-30 13:52:59 +0900

    fix: lint

--- src/pages/demo/draw-colors.vue
+++ src/pages/demo/draw-colors.vue
@@ -18,61 +18,61 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, onUnmounted } from "vue";
-import { useI18n } from "vue-i18n";
-import { useApi, useFromData } from "/~/plugins/axios";
-import { AxiosRequestConfig } from "axios";
-import { flash, EmitTypes } from "/~/plugins/emitter";
+import { ref, computed, watch, onUnmounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { useApi, useFromData } from '/~/plugins/axios'
+import { AxiosRequestConfig } from 'axios'
+import { flash, EmitTypes } from '/~/plugins/emitter'
 
-const { t } = useI18n();
-const isProd = import.meta.env.MODE === "production";
+const { t } = useI18n()
+const isProd = import.meta.env.MODE === 'production'
 
-const image = ref<File | null>(null);
-const resultImage = ref<string>("");
-const showAlern = ref<boolean>(false);
+const image = ref<File | null>(null)
+const resultImage = ref<string>('')
+const showAlern = ref<boolean>(false)
 watch(showAlern, (v) => {
-  if (v === true) setTimeout(() => (v = false), 5000);
-});
+  if (v === true) setTimeout(() => (v = false), 5000)
+})
 
-const url = import.meta.env.VITE_FUNCTIONS_ENDPOINT;
-const drawColorUrl = `${url}/colorization`;
-const allowFileTypes = ["image/jpeg"];
+const url = import.meta.env.VITE_FUNCTIONS_ENDPOINT
+const drawColorUrl = `${url}/colorization`
+const allowFileTypes = ['image/jpeg']
 
-const reader = new FileReader();
+const reader = new FileReader()
 reader.onload = (event) => {
-  resultImage.value = event.target?.result as string;
-};
+  resultImage.value = event.target?.result as string
+}
 
 const config: AxiosRequestConfig = {
   headers: {
-    "Content-Type": "multipart/form-data",
+    'Content-Type': 'multipart/form-data',
   },
-  responseType: "blob",
-};
+  responseType: 'blob',
+}
 
 const { data, loading, error, post, cancel } = useApi<Blob>(
   drawColorUrl,
-  config
-);
+  config,
+)
 
 const checkFileExt = (file: File) => {
-  if (allowFileTypes.includes(file.type)) return true;
-  flash(EmitTypes.Warning, t("errors.not-image"));
-  return false;
-};
+  if (allowFileTypes.includes(file.type)) return true
+  flash(EmitTypes.Warning, t('errors.not-image'))
+  return false
+}
 
 watch(data, (v) => {
-  if (v) reader.readAsDataURL(v as Blob);
-});
+  if (v) reader.readAsDataURL(v as Blob)
+})
 watch(error, (e) => {
-  console.log(e);
-});
+  console.log(e)
+})
 watch(image, (file) => {
-  if (!file || !checkFileExt(file)) return;
-  const formData = useFromData("image", file);
-  post(formData);
-});
+  if (!file || !checkFileExt(file)) return
+  const formData = useFromData('image', file)
+  post(formData)
+})
 onUnmounted(() => {
-  cancel();
-});
+  cancel()
+})
 </script>
