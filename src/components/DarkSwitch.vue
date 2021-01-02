9b697c7e008e97d048ab6b9975148b9a59e8a4e0
hanlee <me@hanlee.co>
2021-01-03 01:36:51 +0900

    feat: add dark mode selector

---
+++ src/components/DarkSwitch.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="focus:outline-none cursor-pointer px-3 md:px-5 text-white h-full" @click="isDark = !isDark">
+    <bx-bx-moon v-if="isDark" class="text-2xl" />
+    <bx-bx-sun v-else class="text-2xl" />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { isDark } from '/~/logics'
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,18 @@
 <template>
-  <header class="flex-none flex items-center justify-between bg-gray-500 dark:bg-gray-600 w-full h-16 basic-transition">
-    <div class="flex h-full items-center">
-      <div class="w-18 h-full bg-gray-700 dark:bg-gray-900 flex justify-center items-center">
-        <img src="/~/assets/logo.svg" width="30" height="30" alt="Icon">
-      </div>
-      <router-link to="/" class="inline-flex text-white">
-        <h1 class="text-3xl font-bold leading-none ml-6">
+  <header class="flex-none flex bg-gray-500 dark:bg-gray-600 w-full h-16 basic-transition">
+    <div class="w-18 h-full bg-gray-700 dark:bg-gray-900 inline-flex justify-center items-center">
+      <img src="/~/assets/logo.svg" width="30" height="30" alt="Icon">
+    </div>
+    <div class="flex-auto flex h-full justify-between items-center mx-4 md:mx-6">
+      <router-link to="/" class="text-white">
+        <h1 class="text-3xl font-bold leading-none">
           OVaaS
         </h1>
       </router-link>
+      <div class="inline-flex h-full items-center">
+        <LanguageSelect />
+        <DarkSwitch class="md:hidden" />
+      </div>
     </div>
-    <LanguageSelect />
   </header>
 </template>
-
-<script setup lang="ts">
-</script>
-
-<style>
-</style>
