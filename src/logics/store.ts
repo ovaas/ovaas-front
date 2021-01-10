943a55afd08c6cbadc8e870080fb80b048fc96bd
hanlee <me@hanlee.co>
2021-01-10 18:26:18 +0900

    fix: typo

--- src/logics/store.ts
+++ src/logics/store.ts
@@ -2,6 +2,6 @@ import { Ref } from 'vue'
 import { useStorage } from '@vueuse/core'
 import { locales } from '../messages'
 
-export const localeSchema = useStorage('locales', '') as Ref<locals>
+export const localeSchema = useStorage('locale', '') as Ref<locals>
 
 export const colorSchema = useStorage('color-schema', 'auto') as Ref<'auto' | 'dark' | 'light'>
