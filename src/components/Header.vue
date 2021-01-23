0cebac7689a5ec5dda06622fd9c01c8e8f67e127
hanlee <me@hanlee.co>
2021-01-24 04:59:52 +0900

    fix: build

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,10 @@
 <template>
   <header class="flex flex-none w-full h-16 bg-gray-500 dark:bg-gray-600 basic-transition">
-    <div class="items-center justify-center hidden h-full bg-gray-700 md:inline-flex w-18 dark:bg-gray-900">
+    <div class="inline-flex items-center justify-center h-full md:bg-gray-700 w-18 md:dark:bg-gray-900">
       <img src="/~/assets/logo.svg" width="30" height="30" alt="Icon">
     </div>
-    <div class="flex items-center justify-between flex-auto h-full mx-4 md:mx-6">
+    <div class="flex items-center justify-between flex-auto h-full mr-3 md:mx-6">
       <router-link to="/" class="inline-flex text-white">
-        <img src="/~/assets/logo.svg" width="25" height="25" alt="Icon" class="mr-4 md:hidden">
         <h1 class="text-3xl font-bold leading-none">
           OVaaS
         </h1>
