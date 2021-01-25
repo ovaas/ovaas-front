7153245183d9be326501704c48cc65869f089372
hanlee <me@hanlee.co>
2021-01-25 12:26:10 +0900

    fix: default language

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -4,15 +4,21 @@ import { messages } from '../messages'
 import { UserModule } from '/~/types'
 import { localeSchema } from '../logics'
 
+const isClient = typeof window !== 'undefined'
+
 // multiple language
 export const install: UserModule = ({ app }) => {
   const preferredLangs = usePreferredLanguages()
   const language = preferredLangs.value.find((locale) => {
-    return Object.keys(messages).includes(locale) || 'ja'
+    return Object.keys(messages).includes(locale) ? locale : 'ja'
   })
 
+  const locale = localeSchema.value ? localeSchema.value : isClient ? language : 'ja'
+
+  console.log(locale)
+
   const i18n = createI18n({
-    locale: localeSchema.value ? localeSchema.value : language,
+    locale,
     messages,
   })
 
