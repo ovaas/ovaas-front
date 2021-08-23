7c2a53d3d19531aaa859a38ae71df765b92c176e
hanlee <me@hanlee.co>
2021-08-23 09:46:14 +0900

    chore: simplify logics

--- src/modules/i18n.ts
+++ src/modules/i18n.ts
@@ -4,16 +4,14 @@ import { UserModule } from '@/types'
 import { messages } from '../messages'
 import { localeSchema } from '../logics'
 
-const isClient = typeof window !== 'undefined'
-
 // multiple language
 export const install: UserModule = ({ app }) => {
   const preferredLangs = usePreferredLanguages()
   const language = preferredLangs.value.find((locale) => {
-    return Object.keys(messages).includes(locale) ? locale : 'ja'
-  })
+    return Object.keys(messages).includes(locale)
+  }) || 'ja'
 
-  const locale = localeSchema.value ? localeSchema.value : isClient ? language : 'ja'
+  const locale = localeSchema.value || language
 
   const i18n = createI18n({
     locale,
