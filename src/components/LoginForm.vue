ca3aeb7b4cb2efad27e358af71c6a842f6ff692b
Aqua Minato <sweet4182691@gmail.com>
2021-09-23 21:56:50 +0900

    "useApi" outside the function

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -19,42 +19,17 @@ const schema: FormSchema = {
   submitText: 'form.login',
 }
 
-const loading = ref(false)
+const { loading, post } = useApi<Blob>(`api/v1/auth`,{
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  responseType: 'blob',
+})
 
 function onSubmit(values: any) {
-  loginUseApi("/api/v1/auth")
   alert(JSON.stringify(values, null, 2))
-}
-
-function loginUseApi(path: string) {
-  const { loading, post} = useApi<Blob>(`${path}`,{
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    responseType: 'blob',
-  })
-
   post({'Password': 'pass'}) // TODO: 入力されたpassを入れる
-
-  return (
-    loading
-  )
 }
-
-// function loginProcess(){
-//   axios.post("/api/v1/auth", {
-//       // TODO: 送信データを決める
-//       password: "password"
-//     })
-//     .then(function(res) {
-//       // TODO: ログイン成功
-//       // TODO: loading refをfalseに更新
-//       console.log(res);
-//     })
-//     .catch(function(err) {
-//       console.error(err)
-//     });
-// }
 
 </script>
 
