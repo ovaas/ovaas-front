1c3e29b53db3c3a1cd21a3e8bcbd38cf52e58392
hanlee <me@hanlee.co>
2021-09-28 16:25:08 +0900

    chore: cleanup

--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="vite-plugin-pages/client" />
-/// <reference types="vite-plugin-icons/client" />
-
-interface ImportMetaEnv {
-  VITE_FUNCTIONS_ENDPOINT: string
-}
