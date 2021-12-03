d3928c1c2ae6c2cd4ff6517c8a50d92a02229677
Yusuke Sakabe <58514643+yusuke-1105@users.noreply.github.com>
2021-12-03 22:20:26 +0900

    feat: demo hide-distant-person (#33)
    * update en.json & ja.json
    
    * add "hide-distant-person" func
    
    画像内の距離に応じて人物の明暗を変更するプログラムの説明文を追加．
    
    * modified locals/ja.yml(hide-distant-person)
    
    * feat: hide-distant-person demo component
    
    * upload hide-distant-person.webp
    
    Co-authored-by: hanlee <me@hanlee.co>

---
+++ src/pages/demo/hide-distant-person.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <DemoImageUpload
+    :title="t('demos.hide-distant-person.title')"
+    :description="t('demos.hide-distant-person.content')"
+    upload-path="/hide-distant-person"
+  />
+</template>
