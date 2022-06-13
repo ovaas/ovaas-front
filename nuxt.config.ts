fa0925d57d958a3e519bb159fd13d460a8c6878f
hanlee <me@hanlee.co>
2022-06-13 19:21:43 +0900

    fix: icon type error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -58,11 +58,13 @@ export default defineNuxtConfig({
           src: '/android-chrome-192x192.png',
           sizes: '192x192',
           type: 'image/png',
+          purpose: 'maskable',
         },
         {
           src: '/android-chrome-512x512.png',
           sizes: '512x512',
           type: 'image/png',
+          purpose: 'maskable',
         },
       ],
     },
